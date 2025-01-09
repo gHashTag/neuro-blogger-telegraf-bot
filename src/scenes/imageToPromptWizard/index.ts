57d6c5e17948a748bfa965c366c0713868f88a30
Dmitriy Vasilev <raoffonom@icloud.com>
2025-01-09 23:04:32 +0700

    imageToPromptWizard

--- src/scenes/imageToPromptWizard/index.ts
+++ src/scenes/imageToPromptWizard/index.ts
@@ -3,9 +3,10 @@ import { MyContext } from '@/interfaces'
 import { imageToPromptCost, sendBalanceMessage } from '@/price/helpers'
 import { generateImageToPrompt } from '@/services/generateImageToPrompt'
 import { BOT_TOKEN } from '@/core/bot'
-import { mainMenu } from '@/menu/mainMenu'
-import { cancelMenu } from '@/menu'
+
+import { createHelpCancelKeyboard } from '@/menu'
 import { getUserBalance } from '@/core/supabase'
+import { handleHelpCancel } from '@/handlers/handleHelpCancel'
 
 if (!process.env.HUGGINGFACE_TOKEN) {
   throw new Error('HUGGINGFACE_TOKEN is not set')
@@ -31,7 +32,7 @@ export const imageToPromptWizard = new Scenes.WizardScene<MyContext>(
         ? 'Пожалуйста, отправьте изображение для генерации промпта'
         : 'Please send an image to generate a prompt',
       {
-        reply_markup: cancelMenu(isRu).reply_markup,
+        reply_markup: createHelpCancelKeyboard(isRu).reply_markup,
       }
     )
     return ctx.wizard.next()
@@ -41,39 +42,29 @@ export const imageToPromptWizard = new Scenes.WizardScene<MyContext>(
     console.log('Waiting for photo message...')
     const imageMsg = ctx.message
 
-    if (imageMsg && 'text' in imageMsg) {
-      if (imageMsg.text.toLowerCase() === (isRu ? 'отмена' : 'cancel')) {
+    const isCancel = await handleHelpCancel(ctx)
+    if (isCancel) {
+      return ctx.scene.leave()
+    } else {
+      if (!imageMsg || !('photo' in imageMsg) || !imageMsg.photo) {
+        console.log('No photo in message')
+        console.log('No photo in message')
         await ctx.reply(
-          isRu
-            ? '❌ Выбор изображения отменен.'
-            : '❌ Selection image cancelled.',
-          {
-            reply_markup: {
-              keyboard: mainMenu(isRu).reply_markup.keyboard,
-            },
-          }
+          isRu ? 'Пожалуйста, отправьте изображение' : 'Please send an image'
         )
         return ctx.scene.leave()
       }
-    }
 
-    if (!imageMsg || !('photo' in imageMsg) || !imageMsg.photo) {
-      console.log('No photo in message')
-      await ctx.reply(
-        isRu ? 'Пожалуйста, отправьте изображение' : 'Please send an image'
-      )
+      const photoSize = imageMsg.photo[imageMsg.photo.length - 1]
+      console.log('Getting file info for photo:', photoSize.file_id)
+      const file = await ctx.telegram.getFile(photoSize.file_id)
+      ctx.session.mode = 'image_to_prompt'
+      const imageUrl = `https://api.telegram.org/file/bot${BOT_TOKEN}/${file.file_path}`
+      if (ctx.from) {
+        await generateImageToPrompt(imageUrl, ctx.from.id, ctx, isRu)
+      }
       return ctx.scene.leave()
     }
-
-    const photoSize = imageMsg.photo[imageMsg.photo.length - 1]
-    console.log('Getting file info for photo:', photoSize.file_id)
-    const file = await ctx.telegram.getFile(photoSize.file_id)
-    ctx.session.mode = 'image_to_prompt'
-    const imageUrl = `https://api.telegram.org/file/bot${BOT_TOKEN}/${file.file_path}`
-    if (ctx.from) {
-      await generateImageToPrompt(imageUrl, ctx.from.id, ctx, isRu)
-    }
-    return ctx.scene.leave()
   }
 )
 
