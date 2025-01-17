5aa55555f0842848adc9843acbe80802fbfeb2a8
Dmitriy Vasilev <raoffonom@icloud.com>
2025-01-17 13:15:37 +0700

    upgrade prompt

--- src/core/openai/upgradePrompt.ts
+++ src/core/openai/upgradePrompt.ts
@@ -8,15 +8,15 @@ export const upgradePrompt = async (prompt: string) => {
         {
           role: 'system',
           content:
-            'You are a helpful assistant that upgrades the given prompt for image generation. Return only the upgraded prompt. ',
+            'You are a helpful assistant that upgrades the given prompt for image generation. Return only the upgraded prompt. Maximum detail and disclosure of meaning. ',
         },
         {
           role: 'user',
           content: prompt,
         },
       ],
-      temperature: 0.3,
-      max_tokens: 1000,
+      temperature: 0.4,
+      max_tokens: 2000,
     })
 
     return completion.choices[0].message.content
